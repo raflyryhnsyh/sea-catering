24cc2e46f2097758a80ec4847191d324d026bb11
raflyryhsnyh <rayhansyahrafly@gmail.com>
2025-06-21 11:03:22 +0700

    style(navbar): menyesuaikan navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,12 +2,11 @@ import { Button } from "@/components/ui/button";
 import { Logo } from "./logo";
 import NavMenu from "./nav-menu";
 import { NavigationSheet } from "./nav-sheet";
-import { ModeToggle } from "@/components/theme/theme-switcher";
+import ThemeToggle from "@/components/theme/theme-switcher";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="fixed top-6 inset-x-4 h-16 bg-background border dark:border-slate-700/70 max-w-screen-xl mx-auto rounded-full">
+    <nav className="fixed top-6 inset-x-4 h-16 bg-background border dark:border-slate-700/70 max-w-screen-xl mx-auto rounded-full">
         <div className="h-full flex items-center justify-between mx-auto px-4">
           <Logo />
 
@@ -15,6 +14,7 @@ const Navbar = () => {
           <NavMenu className="hidden md:block" />
 
           <div className="flex items-center gap-3">
+            <ThemeToggle />
             <Button
               variant="outline"
               className="hidden sm:inline-flex rounded-full"
@@ -22,7 +22,6 @@ const Navbar = () => {
               Sign In
             </Button>
             <Button className="rounded-full">Get Started</Button>
-            <ModeToggle />
 
             {/* Mobile Menu */}
             <div className="md:hidden">
@@ -31,7 +30,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-    </div>
   );
 };
 
--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -1,54 +1,31 @@
 "use client";
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
-import * as SwitchPrimitives from "@radix-ui/react-switch";
-import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
 
-export function ModeToggle() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = React.useState(false);
+const ThemeToggle = () => {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
 
-  React.useEffect(() => {
+  useEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) {
-    return (
-      <div className="h-6 w-11 bg-gray-200 rounded-full animate-pulse"></div>
-    );
+    return <Button variant="outline" size="icon" />;
   }
 
-  const isDark = theme === "dark";
-
-  const handleThemeChange = (checked: boolean) => {
-    setTheme(checked ? "dark" : "light");
-  };
-
   return (
-    <SwitchPrimitives.Root
-      checked={isDark}
-      onCheckedChange={handleThemeChange}
-      className={cn(
-        "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
-        isDark
-          ? "bg-primary data-[state=checked]:bg-primary"
-          : "bg-primary data-[state=unchecked]:bg-primary"
-      )}
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
     >
-      <SwitchPrimitives.Thumb
-        className={cn(
-          "pointer-events-none flex h-5 w-5 items-center justify-center rounded-full bg-background shadow-lg ring-0 transition-transform",
-          isDark ? "translate-x-5" : "translate-x-0"
-        )}
-      >
-        {isDark ? (
-          <Moon className="h-3 w-3 text-primary" />
-        ) : (
-          <Sun className="h-3 w-3 text-primary" />
-        )}
-      </SwitchPrimitives.Thumb>
-    </SwitchPrimitives.Root>
+      {resolvedTheme === "dark" ? <SunIcon /> : <MoonIcon />}
+    </Button>
   );
-}
+};
+
+export default ThemeToggle;
